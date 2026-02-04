f907b73cf32ca5aa87f62c3787fe72c9039fc010
Matthew Corzo <matthew.corzo@estudiantesunibague.edu.co>
2026-02-04 09:52:25 -0500

    Metodos para consignar y retirar terminados

--- src/CuentaBancaria.java
+++ src/CuentaBancaria.java
@@ -14,4 +14,22 @@ public class CuentaBancaria {
     public int getSaldo(){
         return saldo;
     }
+
+    public void consignar(int valorAConsignar){
+        if(valorAConsignar <= 0){
+            throw new IllegalArgumentException("El valor NO es valido");
+        }
+        this.saldo += valorAConsignar;
+    }
+
+    public void retirar(int valorARetirar){
+        if(valorARetirar <= 0){
+            throw new IllegalArgumentException("El valor no es valido");
+        }
+        if(valorARetirar > saldo){
+            throw new IllegalStateException("No se puede retirar mas de lo que cuenta su saldo");
+        }
+
+        this.saldo -= valorAConsignar;
+    }
 }
