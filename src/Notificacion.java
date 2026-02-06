249ad6dab17bdc349165a9b0d33bbfa810c15c7b
Matthew <matthew.corzo@estudiantesunibague.edu.co>
2026-02-06 08:39:25 -0500

    plantilla de notificacion

---
+++ src/Notificacion.java
@@ -0,0 +1,12 @@
+public abstract class Notificacion {
+    protected final String receptorNotificacion;
+
+    public Notificacion(String receptorNotificacion){
+        if(receptorNotificacion == null || receptorNotificacion.isBlank()){
+            throw new IllegalArgumentException("El receptor es invalido");
+        }
+        this.receptorNotificacion = receptorNotificacion;
+    }
+
+    public abstract String enviarMensaje(String mensaje);
+}
